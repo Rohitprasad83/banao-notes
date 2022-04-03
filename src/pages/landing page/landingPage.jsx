c5ec08b0fb2c77d4b416a6b9125e08a518981693
Rohit Prasad <pdrohit674@gmail.com>
2022-04-03 13:30:49 +0530

    feat: added Landing Page

---
+++ src/pages/landing page/landingPage.jsx
@@ -0,0 +1,26 @@
+import notesApp from '../../assets/images/notesApp.svg'
+import landingStyle from './landingPage.module.css'
+export function LandingPage() {
+  return (
+    <div className={landingStyle['container']}>
+      <div className={landingStyle['details']}>
+        <h3 className={landingStyle['heading']}>Banao Notes</h3>
+        <h5>Meet your modern</h5>
+        <h5>Note Taking App</h5>
+        <p className="text__md">
+          Manage your Daily tasks and workflow in a modern way and boost your
+          efficiency without any efforts
+        </p>
+        <button className={`btn btn__warning ${landingStyle['join']}`}>
+          Join Now
+        </button>
+        <p className={`text__md ${landingStyle['alreadyAccount']}`}>
+          Already have an account?
+        </p>
+      </div>
+      <div className={landingStyle['landingImage']}>
+        <img src={notesApp} alt="Landing Page" className="responsive__img" />
+      </div>
+    </div>
+  )
+}
