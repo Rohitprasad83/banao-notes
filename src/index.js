f6d8a4c5193dff23612718036b8a085402385cc9
Rohit Prasad <pdrohit674@gmail.com>
2022-04-03 21:41:11 +0530

    feat: added signup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { AuthContextProvider } from 'context/auth-context.jsx'
 import { makeServer } from './server'
 import { BrowserRouter } from 'react-router-dom'
 // Call make Server
@@ -10,7 +11,9 @@ makeServer()
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
