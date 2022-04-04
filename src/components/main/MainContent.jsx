c9bb82731eaae7e16cd52b3e783835c59867fbb2
Rohit Prasad <pdrohit674@gmail.com>
2022-04-04 23:38:45 +0530

    feat: added open and close create note button

--- src/components/main/MainContent.jsx
+++ src/components/main/MainContent.jsx
@@ -5,6 +5,7 @@ import { Note } from './Note'
 function MainContent() {
   const { note, noteDispatch, notes, setNotes } = useNote()
   const [showPalette, setShowPalette] = useState(false)
+  const [createNote, setCreateNote] = useState(false)
   const colors = ['red', 'blue', 'green', 'yellow', 'black']
   const encodedToken = localStorage.getItem('token')
   const saveNoteHandler = async e => {
@@ -21,6 +22,7 @@ function MainContent() {
       )
       console.log(response)
       setNotes(response.data.notes)
+      setShowPalette(false)
       response.status === 201 && noteDispatch({ type: 'RESET' })
     } catch (err) {
       console.log(err)
@@ -43,67 +45,78 @@ function MainContent() {
     <div>
       <div className="text__lg text__center notes__container">
         <div className="new__note">
-          <button className="btn btn__warning">Create a New Note</button>
+          <button
+            className="btn btn__warning"
+            onClick={() => setCreateNote(true)}>
+            Create a New Note
+          </button>
         </div>
         <div className="notes__container">
-          <div
-            className="note__input"
-            style={{ backgroundColor: note.color.color }}>
-            <input
-              type="text"
-              placeholder="Title"
-              className="input__title"
-              value={note.title}
-              onChange={e =>
-                noteDispatch({ type: 'TITLE', payload: e.target.value })
-              }
-            />
-            <textarea
-              cols="30"
-              rows="10"
-              placeholder="Body of the Note"
-              className="input__body"
-              value={note.body}
-              onChange={e =>
-                noteDispatch({ type: 'BODY', payload: e.target.value })
-              }></textarea>
-            <div className="text__lg input__bottom">
-              <i
-                className="fa-solid fa-palette input__icons palette__container"
-                onClick={() => setShowPalette(!showPalette)}></i>
-              {showPalette && (
-                <div className="palette">
-                  {colors.map((color, index) => (
-                    <div
-                      className="palette__color"
-                      key={index}
-                      style={{ backgroundColor: color }}
-                      onClick={e =>
-                        noteDispatch({ type: 'COLOR', payload: { color } })
-                      }></div>
-                  ))}
-                  <div>
-                    <i
-                      className="fa-solid fa-x cursor-pointer"
-                      onClick={() => setShowPalette(false)}></i>
+          {createNote && (
+            <div
+              className="note__input"
+              style={{ backgroundColor: note.color.color }}>
+              <input
+                type="text"
+                placeholder="Title"
+                className="input__title"
+                value={note.title}
+                onChange={e =>
+                  noteDispatch({ type: 'TITLE', payload: e.target.value })
+                }
+              />
+              <textarea
+                cols="30"
+                rows="10"
+                placeholder="Body of the Note"
+                className="input__body"
+                value={note.body}
+                onChange={e =>
+                  noteDispatch({ type: 'BODY', payload: e.target.value })
+                }></textarea>
+              <div className="text__lg input__bottom">
+                <i
+                  className="fa-solid fa-palette input__icons palette__container"
+                  onClick={() => setShowPalette(!showPalette)}></i>
+                {showPalette && (
+                  <div className="palette">
+                    {colors.map((color, index) => (
+                      <div
+                        className="palette__color"
+                        key={index}
+                        style={{ backgroundColor: color }}
+                        onClick={e =>
+                          noteDispatch({ type: 'COLOR', payload: { color } })
+                        }></div>
+                    ))}
+                    <div>
+                      <i
+                        className="fa-solid fa-x cursor-pointer"
+                        onClick={() => setShowPalette(false)}></i>
+                    </div>
                   </div>
-                </div>
-              )}
-              <i className="fa-solid fa-tag input__icons"></i>
-              <i
-                className="fa-solid fa-box-archive input__icons"
-                style={{ color: note.archive ? 'var(--warning)' : 'black' }}
-                onClick={e => noteDispatch({ type: 'ARCHIVE' })}></i>
-              <i
-                className="fa-solid fa-trash input__icons"
-                onClick={e => noteDispatch({ type: 'RESET' })}></i>
-              <button className="btn btn__warning" onClick={saveNoteHandler}>
-                Save
-              </button>
+                )}
+                <i className="fa-solid fa-tag input__icons"></i>
+                <i
+                  className="fa-solid fa-box-archive input__icons"
+                  style={{ color: note.archive ? 'var(--warning)' : 'black' }}
+                  onClick={e => noteDispatch({ type: 'ARCHIVE' })}></i>
+                <i
+                  className="fa-solid fa-trash input__icons"
+                  onClick={e => noteDispatch({ type: 'RESET' })}></i>
+                <button className="btn btn__warning" onClick={saveNoteHandler}>
+                  Save
+                </button>
+                <button
+                  className="btn btn__error"
+                  onClick={() => setCreateNote(false)}>
+                  Close
+                </button>
+              </div>
             </div>
-          </div>
+          )}
           {notes.map(({ _id, title, body, color }) => (
-            <Note key={_id} title={title} body={body} color={color} />
+            <Note key={_id} id={_id} title={title} body={body} color={color} />
           ))}
         </div>
       </div>
--- src/components/main/Note.jsx
+++ src/components/main/Note.jsx
@@ -1,5 +1,21 @@
 import { useNote } from 'context/index'
-export const Note = ({ title, body, color }) => {
+import axios from 'axios'
+
+export const Note = ({ id, title, body, color }) => {
+  const encodedToken = localStorage.getItem('token')
+  const { setNotes } = useNote()
+  const deleteNoteHandler = async e => {
+    e.preventDefault()
+    try {
+      const response = await axios.delete(`/api/notes/${id}`, {
+        headers: { authorization: encodedToken },
+      })
+      console.log(response)
+      setNotes(response.data.notes)
+    } catch (err) {
+      console.log(err)
+    }
+  }
   return (
     <div className="note" style={{ backgroundColor: color.color }}>
       <div className="note__title">{title}</div>
@@ -8,7 +24,9 @@ export const Note = ({ title, body, color }) => {
         <i className="fa-solid fa-palette input__icons"></i>
         <i className="fa-solid fa-tag input__icons"></i>
         <i className="fa-solid fa-box-archive input__icons"></i>
-        <i className="fa-solid fa-trash input__icons"></i>
+        <i
+          className="fa-solid fa-trash input__icons"
+          onClick={deleteNoteHandler}></i>
         <button className="btn btn__warning">Save</button>
         <button className="btn btn__error">Edit</button>
       </div>
