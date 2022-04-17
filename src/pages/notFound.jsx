e1c3ab59028cfad2084b05749dc8df7e2417a062
Rohit Prasad <pdrohit674@gmail.com>
2022-04-18 00:02:44 +0530

    feat: 404 page

---
+++ src/pages/notFound.jsx
@@ -0,0 +1,25 @@
+// import { Navbar, Footer } from 'components/index'
+import { Layout } from 'components/index'
+import { useTitle } from 'utils/useTitle'
+import error404 from 'assets/images/error404.png'
+export function Error404() {
+  useTitle(' 404')
+  return (
+    <div className="home__container">
+      {/* <Navbar /> */}
+      <Layout />
+
+      <div className={`main__container `}>
+        <h2 className="text__center">
+          We Could not Found What you are Looking For!!!
+        </h2>
+        <img
+          src={error404}
+          alt="Not Found"
+          className="responsive__img error404"
+        />
+      </div>
+      {/* <Footer /> */}
+    </div>
+  )
+}
